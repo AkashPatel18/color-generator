78b021edd0247275cf450e938b9aedbc1de7556b
Akash <akashpatel18041999@gmail.com>
2020-12-10 23:32:50 +0530

    Updates

---
+++ precache-manifest.517eccb0a46b68ef83be300854f9da50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a1e358905aaf7937f7ec81ef0fbf84",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "d5ebb9cd62cbca9938b5",
+    "url": "/color-generator/static/css/main.569331d7.chunk.css"
+  },
+  {
+    "revision": "285d7f1e194478b25880",
+    "url": "/color-generator/static/js/2.e5616ca0.chunk.js"
+  },
+  {
+    "revision": "1584b1d83a9a044a3483bb3c9804e9fe",
+    "url": "/color-generator/static/js/2.e5616ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ebb9cd62cbca9938b5",
+    "url": "/color-generator/static/js/main.5fd5686a.chunk.js"
+  },
+  {
+    "revision": "ed92ef5652ce8f57a79c",
+    "url": "/color-generator/static/js/runtime-main.c4c3dd28.js"
+  }
+]);
